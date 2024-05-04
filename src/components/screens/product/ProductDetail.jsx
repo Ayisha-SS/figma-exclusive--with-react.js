34cc3c3f0e19f834d54b6c629ed6bd83829cae33
Ayisha-SS <13aiysh246@gmail.com>
2024-05-04 12:57:18 +0530

    update

--- src/components/screens/product/ProductDetail.jsx
+++ src/components/screens/product/ProductDetail.jsx
@@ -45,7 +45,7 @@ function ProductDetail() {
         window.scrollTo(0, 0);
     }, []);
 
-        const product = data.find((item) => item.id === parseInt(id));
+    const product = data.find((item) => item.id === parseInt(id));
   return (
     <>
         <div className='py-10'>
@@ -53,29 +53,31 @@ function ProductDetail() {
                 <div>
                     <h4 className='text-sm leading-5 font-normal text-[#000]'>Account / { product.type} / <span className='text-[#000]'>{product.name}</span></h4>
                 </div>
-                <div className='flex py-10 mb-12'>
-                    <div className='flex '>
+                <div className='flex py-10 mb-12 md:flex-wrap md:gap-y-8'>
+                    <div className='flex lg:w-[50%] md:w-full'>
                         <div className='flex flex-col gap-4 items-center justify-center'>
                             {[product.image, product.img2, product.img3, product.img4].map((image, index) => (
                                 <div
                                     key={index}
-                                    className="bg-[#00000014] w-[170px] h-[138px] rounded cursor-pointer flex items-center justify-center"
+                                    className="bg-[#00000014] w-[170px] h-[138px] rounded cursor-pointer flex items-center justify-center lg:w-[120px] lg:h-[128px] md:w-[170px]"
                                     onClick={() => handleImageClick(image)}
                                 >
-                                    <div className='w-[50%]'>
+                                    <div className='w-[50%] '>
                                         <img src={require(`../../../${image}`)} alt={`Product Image ${index}`} />
                                     </div>
                                 </div>
                             ))}
-
                         </div>
-                        <div className='bg-[#00000014] rounded flex items-center justify-center w-[500px] h-[600px] ml-6 lg:w-[px] '>
-                            <div className='w-[90%]'>
-                                <img src={require(`../../../${selectedImage || product.image}`)} alt={product.name} />
+                        <div className='md:w-full'>
+                            <div className='bg-[#00000014] rounded flex items-center justify-center w-[500px] h-[600px] ml-6 lg:w-[300px] lg:h-[550px] md:w-[500px]'>
+                                <div className='w-[90%]'>
+                                    <img src={require(`../../../${selectedImage || product.image}`)} alt={product.name} />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-col px-12 w-[50%]'>
+
+                    <div className='flex flex-col px-12 md:px-0 w-[50%] md:flex-wrap'>
                         <div className='flex flex-col gap-4 '>
                             <h3 className='text-2xl leading-6 font-semibold'>{product.name}</h3>
                             <div className='flex gap-4 items-center'>
@@ -91,9 +93,9 @@ function ProductDetail() {
                                 <span className='text-sm leading-5 font-normal  text-black'>In Stock</span>
                             </div>
                             <h4 className='text-2xl leading-6 font-normal '>$ {product.price * count}</h4>
-                            <span className='text-sm leading-5 font-normal mb-8 text-[#000] w-[70%]'>PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</span>
+                            <span className='text-sm leading-5 font-normal mb-8 text-[#000] w-[70%] md:w-full'>PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.</span>
                         </div>
-                        <hr className='w-[80%]'/>
+                        <hr className='w-[80%] md:w-full'/>
                         <div className='flex flex-col gap-8 mt-8'>
                             <div className='flex gap-6 items-center'>
                                 <span className='text-xl leading-5 font-normal'>Size:</span>
@@ -103,7 +105,7 @@ function ProductDetail() {
                                 <button className='text-sm leading-5 font-medium border border-[#000] rounded w-8 h-8'>L</button>
                                 <button className='text-sm leading-5 font-medium border border-[#000] rounded w-8 h-8'>XL</button>
                             </div>
-                            <div className='flex gap-4 mb-3'>
+                            <div className='flex gap-4 mb-3 '>
                                 <div className='flex items-center'>
                                     <div className='px-4 py-2 rounded-l border border-[#5e5d5d] cursor-pointer' onClick={decreaseCount}>-</div>
                                     <div className='px-8 py-2 border border-[#5e5d5d] '>{count}</div>
@@ -143,6 +145,8 @@ function ProductDetail() {
                         </div>
                     </div>
                 </div>  
+
+            {/*..................... related items............. */}
                 <div className='pb-10'>
                     <div className='flex items-center'>
                         <svg width={40} height={40}>
